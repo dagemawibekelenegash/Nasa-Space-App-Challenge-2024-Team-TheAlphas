013d6b75fcf979c7e380e4aea507c6516a348787
dagemawinegash <dagemawibekele04@gmail.com>
2024-10-06 23:29:24 +0300

    final touches

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Nasa-Space-App-Challenge-2024-Team-TheAlphas/',  
+  plugins: [react()] 
 });
